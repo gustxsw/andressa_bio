dde54b18d8347fec294d854166912d6770f8950e
gustxsw <gustavocandido044@gmail.com>
2025-01-16 15:12:47 -0300

    erro_corrigido

--- send_mail.php
+++ send_mail.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Capture os dados  do formulário
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message'])
+    $message = htmlspecialchars($_POST['message']);
 
     // Configurações do email
     $to = "gustavocandido044@gmail.com";
